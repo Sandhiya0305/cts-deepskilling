2ba50882f2139b08ba7ccc7485dcc29a66402ee3
Sandhiya0305 <sandhiyarajkumar0305@gmail.com>
2026-07-13 11:56:02 +0530

    Solved react ex-02 studentapp

---
+++ Module-09-React/studentapp/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './Component/Home';
+import About from './Component/About';
+import Contact from './Component/Contact';
+
+
+function App() {
+  return (
+    <div className="Container">
+    <Home/>
+    <About/>
+    <Contact/>
+    </div>
+  );
+}
+
+export default App;
