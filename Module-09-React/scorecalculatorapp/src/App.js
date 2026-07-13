82f7b08169c5d0def8428817e656d823bd935467
Sandhiya0305 <sandhiyarajkumar0305@gmail.com>
2026-07-13 14:38:29 +0530

    solved react ex-03 scorecalculator app

---
+++ Module-09-React/scorecalculatorapp/src/App.js
@@ -0,0 +1,13 @@
+import {CalculateScore} from './Components/CalculateScore'
+
+function App() {
+  return (
+    <div>
+      <CalculateScore Name = {"Sherin"}
+      School = {"Don Bosco School"}
+      total = {450}
+      goal = {8}/>
+    </div>
+  );
+}
+export default App;
